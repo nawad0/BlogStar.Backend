55c2b28f9726a12fdb79a41e6ce3d8300f38533e
nawado <doarmax.max@gmail.com>
2023-12-04 19:21:58 +0300

    Добавьте файлы проекта.

---
+++ BlogStar.Backend/Data/BlogStarDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BlogStar.Backend.Data
+{
+    public class BlogStarDbContext : DbContext
+    {
+        
+            public BlogStarDbContext(DbContextOptions options) : base(options)
+            {
+
+            }
+            //public DbSet<Contact> Contacts { get; set; }
+        
+    }
+}
